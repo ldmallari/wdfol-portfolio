7d537f28e579ac37dabf79415257743675c0c3dc
Yuji <mallarilevin@gmail.com>
2025-01-18 19:59:27 +0800

    hero

---
+++ src/pages/Home.jsx
@@ -0,0 +1,102 @@
+import Nav from './Nav';
+import '../assets/styles/home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+export default function Home() {
+    return (
+        <>
+            <Nav/>
+            <div className="relative bg-white pb-[110px] pt-[120px] dark:bg-dark lg:pt-[150px]" id="heropage">
+                <div className="container">
+                <div className="-mx-4 flex flex-wrap">
+                    <div className="w-full px-4 lg:w-5/12">
+                    <div className="hero-content">
+                        <h1 className="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-1xl">
+                        I am Levin
+                        </h1>
+                        <h1 className='herohdr'>Full-Stack Web Developer</h1>
+                        <p className="mb-8 max-w-[480px] text-base text-body-color dark:text-dark-6">
+                        I break down complex user experience problems to create integrity focussed solutions that connect billions of people
+                        </p>
+                        <ul id="hero-btns" className="flex flex-wrap items-center">
+                        <li>
+                            <a
+                            href="/#"
+                            id="about-btn" className="inline-flex items-center justify-center rounded-md bg-primary px-6 py-3 text-center text-base font-normal text-white hover:bg-blue-dark lg:px-7"
+                            >
+                            About me
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                            href="../src/assets/files/resume.txt"
+                            id="dl-resume" className="inline-flex items-center justify-center px-5 py-3 text-center text-base font-normal text-[#464646] hover:text-primary dark:text-white"
+                            download>
+                            Download Resume
+                            </a>
+                        </li>
+                        </ul>
+                        <div className="clients pt-10">
+                        <div className="flex items-center space-x-4" id='social-i'>
+                            <FontAwesomeIcon icon={faFacebookF} className="facebook"/>
+                            <FontAwesomeIcon icon={faGithub} className="github"/>
+                            <FontAwesomeIcon icon={faLinkedinIn} className="linkedin"/>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="hidden px-4 lg:block lg:w-1/12"></div>
+                    <div className="w-full px-4 lg:w-6/12">
+                    <div className="lg:ml-auto lg:text-right">
+                        <div id="hero-img" className="relative z-10 inline-block pt-11 lg:pt-0">
+                        <img
+                            src="../src/assets/images/hero.jpg"
+                            alt="hero"
+                            className="max-w-auto lg:ml-auto"
+                        />
+                        <span className="absolute -top-25 -left-8 z-[-1]" id='test-anim'>
+                            <svg
+                            width="93"
+                            height="93"
+                            viewBox="0 0 93 93"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            >
+                            <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
+                            <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
+                            <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
+                            <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
+                            <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
+                            <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
+                            <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
+                            <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
+                            <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
+                            <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
+                            <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
+                            <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
+                            <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
+                            <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
+                            <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
+                            <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
+                            <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
+                            <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
+                            <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
+                            <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
+                            <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
+                            <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
+                            <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
+                            <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
+                            <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+                            </svg>
+                        </span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </>
+    )
+  }
+  
---
+++ src/pages/Nav.jsx
@@ -0,0 +1,71 @@
+import '../assets/styles/nav.css'
+import React, { useState } from "react";
+const Nav = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className={`flex w-full items-center bg-white dark:bg-dark`}>
+      <div className="container">
+        <div className="relative -mx-4 flex items-center justify-between">
+          <div className="w-60 max-w-full px-4">
+            <a href="/#" className="block w-full py-5">
+              <img
+                src="../src/assets/images/logo.png"
+                alt="logo"
+                className="hidden dark:block"
+              />
+            </a>
+          </div>
+          <div className="flex w-full items-center justify-between px-4">
+            <div>
+              <button
+                onClick={() => setOpen(!open)}
+                id="navbarToggler"
+                className={` ${
+                  open && "navbarTogglerActive"
+                } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
+              >
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+              </button>
+              <nav
+                // :className="!navbarOpen && 'hidden' "
+                id="navbarCollapse"
+                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:dark:bg-transparent ${
+                  !open && "hidden"
+                } `}
+              >
+                <ul className="block lg:flex">
+                  <ListItem NavLink="/#">About</ListItem>
+                  <ListItem NavLink="/#">Services</ListItem>
+                  <ListItem NavLink="/#">Works</ListItem>
+                  <ListItem NavLink="/#">Skills</ListItem>
+                  <ListItem NavLink="/#">Testimonials</ListItem>
+                  <ListItem NavLink="/#">Contact</ListItem>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Nav;
+
+const ListItem = ({ children, NavLink }) => {
+  return (
+    <>
+      <li>
+        <a
+          href={NavLink}
+          id="nav-huver" className="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-white lg:ml-12 lg:inline-flex"
+        >
+          {children}
+        </a>
+      </li>
+    </>
+  );
+};
