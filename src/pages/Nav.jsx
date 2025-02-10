2d13a9e5e51b0d5f6f4875a0d43c4c121a69a708
Yuji <mallarilevin@gmail.com>
2025-02-10 22:38:09 +0800

    link

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,5 +1,6 @@
 import '../assets/styles/nav.css'
 import React, { useState } from "react";
+import { Link } from "react-router";
 const Nav = () => {
   const [open, setOpen] = useState(false);
 
@@ -59,12 +60,12 @@ const ListItem = ({ children, NavLink }) => {
   return (
     <>
       <li>
-        <a
-          href={NavLink}
+        <Link
+          to={NavLink}
           id="nav-huver" className="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-white lg:ml-12 lg:inline-flex"
         >
           {children}
-        </a>
+        </Link>
       </li>
     </>
   );
