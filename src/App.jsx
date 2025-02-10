c219069896dc0992b31f2cb0f3b58e3aa49298c4
Yuji <mallarilevin@gmail.com>
2025-02-10 22:15:36 +0800

    few tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Contact from './pages/contact';
+import Contact from './pages/Contact';
 import About from './pages/About';
 
 function App() {
