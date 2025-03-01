528b19c31d00f8a4cf6582db3d7ac4f252928f5f
Yuji <mallarilevin@gmail.com>
2025-03-01 17:21:32 +0800

    designed project page template

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,12 @@ import { useParams } from "react-router-dom";
 import projects from "../components/projects";
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Nav from "./Nav";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Projects = () => {
     const { pId } = useParams();
@@ -15,6 +21,7 @@ const Projects = () => {
 
   return (
     <>
+    <Nav/>
     <div className="projMain">
       <div className="projContainer">
         <div className="projImage">
@@ -22,16 +29,67 @@ const Projects = () => {
         </div>
 
         <div className="projTxt-Btn">
-          <div>
+          <div className="projCol1">
               <h1>{project.title}</h1>
+              <p>{project.date}</p>
               <p>{project.shortdesc}</p>
+              <div className="liveBtn">
+                <a href={project.liveLink} target="_blank">Live Preview</a><FontAwesomeIcon icon={faArrowRight} className="faArrowproj"/>
+              </div>
           </div>
-          <div className="liveBtn">
-            <a href={project.liveLink} target="_blank">Live Preview</a><FontAwesomeIcon icon={faArrowRight} className="faArrowproj"/>
+
+          <div className="projCol2">
+            <div>
+              <p>Client</p>
+              <p>{project.category}</p>
+            </div>
+            <div>
+              <p>Client</p>
+              <p>{project.category}</p>
+            </div>
           </div>
+
         </div>
 
         <div className="projAlbum">
+        <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
         </div>
 
         <div>
